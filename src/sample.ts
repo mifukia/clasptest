9e6de1a8bd298397eeec940133b996075b94b236
shimoji_akifumi <shimoji_akifumi@13PCM-11.local>
2020-04-03 18:58:20 +0900

    update

--- src/sample.ts
+++ src/sample.ts
@@ -1,3 +1,15 @@
-function test(){
-    Logger.log('test3');
+function myFunction(){
+    // const ss = SpreadsheetApp.getActiveSpreadsheet();
+    // const sheet = ss.getSheetByName('シート１');
+    const sheet = SpreadsheetApp.getActiveSheet();
+    const lastRow = sheet.getLastRow();
+    const array_A = sheet.getRange(`A2:A${lastRow}`).getValues();
+    array_A.map((value,index) => {
+        const rangeIsSent = sheet.getRange(index + 2,4);
+        if(!rangeIsSent.getValue()){
+            console.log(value);
+            rangeIsSent.setValue(true);
+        }
+    });
+    //console.log(array_A);
 }
